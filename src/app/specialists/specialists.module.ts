615e95e9abe1ff86c5124f1b37f20b72ec83c475
JulianCode <hijuliansosa@protonmail.com>
2020-06-26 18:38:47 -0500

    UI: Markup: Specialist layout

--- src/app/specialists/specialists.module.ts
+++ src/app/specialists/specialists.module.ts
@@ -5,10 +5,11 @@ import { SpecialistsRoutingModule } from './specialists-routing.module';
 import { SpecialistsComponent } from './components/specialists/specialists.component';
 import { SpecialistComponent } from './components/specialist/specialist.component';
 import { SpecialistDetailComponent } from './components/specialist-detail/specialist-detail.component';
+import { SpecialistInfoComponent } from './components/specialist-info/specialist-info.component';
 
 
 @NgModule({
-  declarations: [SpecialistsComponent, SpecialistComponent, SpecialistDetailComponent],
+  declarations: [SpecialistsComponent, SpecialistComponent, SpecialistDetailComponent, SpecialistInfoComponent],
   imports: [
     CommonModule,
     SpecialistsRoutingModule
